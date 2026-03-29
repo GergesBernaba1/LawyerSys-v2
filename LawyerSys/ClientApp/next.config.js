084551a02c8fd25936015fcd6ab20aaa896327f9
gergesbernaba1 <gergesbernaba2@gmail.com>
2026-03-29 10:45:11 +0200

    Add deployment script and configuration for local environment setup

--- LawyerSys/ClientApp/next.config.js
+++ LawyerSys/ClientApp/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: false,
   images: {
     domains: ['localhost', process.env.NEXT_PUBLIC_BACKEND_HOST || 'localhost'],
